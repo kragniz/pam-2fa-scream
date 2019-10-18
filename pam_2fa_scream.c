e022540c72950f936fd8d6655d68e32bdcb905fb
Louis Taylor <louis@kragniz.eu>
2019-10-18 04:14:00 +0100

    Listen to the screams

--- pam_2fa_scream.c
+++ pam_2fa_scream.c
@@ -1,16 +1,75 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
 
+#include <alsa/asoundlib.h>
+
+static float check_loudyness(void) {
+    float result = 0.0f;
+    snd_pcm_t *waveform;
+
+    if (snd_pcm_open(&waveform, "default", SND_PCM_STREAM_CAPTURE, 0) != 0)
+        return 0;
+
+    if (!snd_pcm_set_params(waveform, SND_PCM_FORMAT_S16_LE,
+                            SND_PCM_ACCESS_RW_INTERLEAVED, 2, 48000, 1, 0)) {
+        short buffer[256];
+        if (snd_pcm_readi(waveform, buffer, 128) == 128) {
+            for (int i = 0; i < 256; ++i) {
+                float s = buffer[i] / 32768.0f;
+                if (s < 0) {
+                    s *= -1;
+                }
+                if (result < s) {
+                    result = s;
+                }
+            }
+        }
+    }
+
+    snd_pcm_close(waveform);
+    return result;
+}
+
+static bool check_for_big_loud(float big_loud_limit) {
+    bool is_big_loud = false;
+    float loudest = 0;
+
+    for (size_t i = 0; i < 100; i++) {
+        float loud = check_loudyness();
+        printf("level: %f\n", loud);
+        if (loud > loudest) {
+            loudest = loud;
+        }
+        if (loud > big_loud_limit) {
+            is_big_loud = true;
+            break;
+        }
+        usleep(10000);
+    }
+
+    if (!is_big_loud) {
+        printf(
+            "Auth failed: scream louder (%.0f required, only recieved %.0f)\n",
+            big_loud_limit * 100, loudest * 100);
+    }
+    return is_big_loud;
+}
+
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
                         const char **argv) {
     puts("scream pam_sm_authenticate");
-    return (PAM_IGNORE);
+    if (check_for_big_loud(0.3)) {
+        return PAM_SUCCESS;
+    } else {
+        return PAM_AUTH_ERR;
+    }
 }
 
 int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     puts("scream pam_sm_setcred");
-    return (PAM_IGNORE);
+    return PAM_IGNORE;
 }
-
