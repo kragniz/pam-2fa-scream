45832ba8f675c78d3673475bed64b9123434d7f3
Louis Taylor <louis@kragniz.eu>
2019-10-18 03:11:56 +0100

    trim

--- pam_2fa_scream.c
+++ pam_2fa_scream.c
@@ -1,30 +1,7 @@
-#define PAM_SM_ACCOUNT
-#define PAM_SM_AUTH
-#define PAM_SM_PASSWORD
-#define PAM_SM_SESSION
-
 #include <stdio.h>
 
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
-
-int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc,
-                        const char **argv) {
-    puts("scream pam_sm_open_session");
-    return (PAM_IGNORE);
-}
-
-int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc,
-                         const char **argv) {
-    puts("scream pam_sm_close_session");
-    return (PAM_IGNORE);
-}
-
-int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc,
-                     const char **argv) {
-    puts("scream pam_sm_acct_mgmt");
-    return (PAM_IGNORE);
-}
 
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc,
                         const char **argv) {
@@ -37,8 +14,3 @@ int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     return (PAM_IGNORE);
 }
 
-int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc,
-                     const char **argv) {
-    puts("scream pam_sm_chauthtok");
-    return (PAM_IGNORE);
-}
